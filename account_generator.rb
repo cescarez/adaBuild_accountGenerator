847d91becec50f397db810838f3821625bfb1914
Christabel Escarez <christabel.escarez@gmail.com>
2020-08-19 12:00:00 -0700

    Implmented student ID array

--- account_generator.rb
+++ account_generator.rb
@@ -3,21 +3,23 @@
 #Ada Developer's Academy C14
 #Ada Build: Section 6 Assessment: question  l, "Account Generator" 
 
-#MAIN
+#CONSTANTS
 NUM_STUDENTS = 5
 
-student_names = []
-student_ids = []
-student_emails = []
-
+#MAIN
 puts "Please enter the full names of #{NUM_STUDENTS} students. "
+student_names = []
 i = 0
 NUM_STUDENTS.times do 
-  print "Student #{i}: "
-  student_names << gets.chomp
+  print "Student #{i + 1}: "
+  student_names << gets.chomp.strip.upcase
   until student_names[i].include?(' ')
     puts "Entry was not accepted. Please enter both first and last name."
-    student_names[i] = gets.chomp
+    student_names[i] = gets.chomp.strip.upcase
   end
   i += 1
 end
+
+student_ids = Array.new(NUM_STUDENTS) { rand(111111..999999) }
+
+student_emails = []
