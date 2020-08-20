2aaf9e4599093c0ed911266c7c8d25090c750073
Christabel Escarez <christabel.escarez@gmail.com>
2020-08-19 19:42:37 -0700

    Student names, IDs, emails contained within student_info hash. Emails implementations still spit and duct tape.

--- account_generator.rb
+++ account_generator.rb
@@ -6,32 +6,33 @@
 #CONSTANTS
 NUM_STUDENTS = 5
 
+student_info = Hash.new()
 #MAIN
 puts "Please enter the full names of #{NUM_STUDENTS} students. "
-student_names = []
 i = 0
+student_info[:names] = Array.new 
 NUM_STUDENTS.times do 
   print "Student #{i + 1}: "
-  student_names << gets.chomp.strip.upcase
-  until student_names[i].include?(' ')
+  student_info[:names] << gets.chomp.strip.upcase
+  until student_info[:names][i].include?(' ')
     puts "Entry was not accepted. Please enter both first and last name."
-    student_names[i] = gets.chomp.strip.upcase
+    student_info[:names][i] = gets.chomp.strip.upcase
   end
   i += 1
 end
 
-student_ids = Array.new(NUM_STUDENTS) { rand(111111..999999) }
+student_info[:ids] = Array.new(NUM_STUDENTS) { rand(111111..999999) }
 
-student_emails = Array.new 
+student_info[:emails] = Array.new 
 
-student_names.each_with_index do |name, i|
-  split_name = student_names[i].split(' ')
-  split_id = student_ids[i].to_s.split('')
-  student_emails << student_names[i][0] + split_name[-1] + split_id[-3] + split_id[-2] + split_id[-1] + "@adadevelopersacademy.org"
+student_info[:names].each_with_index do |name, i|
+  split_name = student_info[:names][i].split(' ')
+  split_id = student_info[:ids][i].to_s.split('')
+  student_info[:emails] << student_info[:names][i][0] + split_name[-1] + split_id[-3] + split_id[-2] + split_id[-1] + "@adadevelopersacademy.org"
 end
 
-student_names.each_with_index do |name, i|
+student_info[:names].each_with_index do |name, i|
   puts "Name: #{name}"
-  puts "ID: #{student_ids[i]}"
-  puts "email: #{student_emails[i]}"
+  puts "ID: #{student_info[:ids][i]}"
+  puts "email: #{student_info[:emails][i]}"
 end
